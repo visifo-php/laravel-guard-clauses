0d1ee2455cd82d641324cf8d8f2b742cc3b82739
Sergej-Tihonov <Sergej-Tihonov@users.noreply.github.com>
2021-12-19 22:37:08 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Visifo\GuardClauses\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Visifo\GuardClauses\GuardServiceProvider;
 
